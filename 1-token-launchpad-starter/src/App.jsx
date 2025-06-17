69a320f2bf9c0eb5cc11047af359b39acfde167a
Basanta <basantaparajuli.11@gmail.com>
2025-06-17 06:43:21 +0545

    token launch pad

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,27 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import "./App.css";
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+// wallet adapter imports
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    <ConnectionProvider endpoint="https://api.devnet.solana.com">
+      <WalletProvider wallets={[]}>
+        <WalletModalProvider>
+          <WalletMultiButton />
+          <TokenLaunchpad />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,17 +1,102 @@
+import {
+  createInitializeMint2Instruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
 export function TokenLaunchpad() {
-    return  <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+  const wallet = useWallet();
+  const { connection } = useConnection();
+
+  // createToken function to create a new token
+  // It will create a new mint account and initialize it with the provided parameters
+  async function createToken() {
+    const name = document.getElementById("name");
+    const symbol = document.getElementById("symbol");
+    const imageUrl = document.getElementById("imageUrl");
+    const initialSupply = document.getElementById("initialSupply");
+
+    const mintKeypair = Keypair.generate();
+
+    const metadata = {
+      mint: mintKeypair.publicKey,
+      name: name.value,
+    };
+
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mintKeypair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMint2Instruction(
+        mintKeypair.publicKey,
+        9,
+        wallet.publicKey,
+        wallet.publicKey,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    transaction.feePayer = wallet.publicKey;
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    transaction.partialSign(mintKeypair);
+    let response = await wallet.sendTransaction(transaction, connection);
+    console.log(response);
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Solana Token Launchpad</h1>
+      <input
+        id="name"
+        className="inputText"
+        type="text"
+        placeholder="Name"
+      ></input>{" "}
+      <br />
+      <input
+        id="symbol"
+        className="inputText"
+        type="text"
+        placeholder="Symbol"
+      ></input>{" "}
+      <br />
+      <input
+        id="imageUrl"
+        className="inputText"
+        type="text"
+        placeholder="Image URL"
+      ></input>{" "}
+      <br />
+      <input
+        id="initialSupply"
+        className="inputText"
+        type="text"
+        placeholder="Initial Supply"
+      ></input>{" "}
+      <br />
+      <button onClick={createToken} className="btn">
+        Create a token
+      </button>
     </div>
+  );
 }
